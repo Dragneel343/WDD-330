1d43908dc95044d5fdba429d0664bf486d1ba1a2
Dragneel343 <37350944+Dragneel343@users.noreply.github.com>
2021-01-09 14:49:57 -0700

    changes to index, added main.css, and main.js

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+let ol = document.querySelector('ol');
+
+const links = [
+    {
+        label: "Week 1 Notes",
+        url: "week1/index.html"
+    }
+];
+
+links.forEach(
+    link => {
+        ol.innerHTML +=
+        `<li><a href="${link.url}">${link.label}</a></li>`;
+    }
+);
